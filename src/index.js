e6736803717674d6505d2ece71d402c42ef57afa
JIMENA VILLALOBOS <jimenavillalobos@MacbookPROdeDALIA.local>
2022-12-09 14:05:02 -0600

    card validator Jime Villalobos

--- src/index.js
+++ src/index.js
@@ -1,3 +1,50 @@
-import validator from './validator.js';
+import validator from "./validator.js";
 
 console.log(validator);
+
+validator.maskify()
+
+//console.log(validator.mena)
+
+document.getElementById("maskify").addEventListener("click",isValid);
+
+document.getElementById("maskify").addEventListener("click",validator.maskify);
+
+document.addEventListener("input",showCard);
+function showCard(event){
+  console.log(event.target.value);
+  document.getElementById("maskedNumber").innerText=event.target.value;
+//maskedNumber es mi boton guardar
+}
+
+function isValid(){ //Cambiar handleValidate por isValid porque así pide la descripción
+  
+  let showIfIsValid=validator.validateCard();
+  let message="";
+  let backgroundColor="";
+  let border="";
+  console.log(showIfIsValid);
+
+  
+  let validCard=document.getElementById("validOrInvalid");
+  if (showIfIsValid) {
+    message=" ✅ ¡Tarjeta válida! 🥳"
+    backgroundColor="#8af293"
+    border="2px solid #0d7504"
+  
+  } else{
+    message="🚫 Lo sentimos, tu tarjeta no es válida 😢"
+    backgroundColor="#f2988a"
+    border="2px solid #9e1b06"
+  }
+
+    validCard.innerText=message;
+    validCard.style.backgroundColor=backgroundColor;
+    validCard.style.display="block";
+    validCard.style.border=border;
+}
+
+
+
+
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,75 @@
+
 const validator = {
-  // ...
+  validateCard:function(){
+    let cardArray=document.getElementById("cardNumber").value;
+    cardArray=cardArray.split("");
+    let cardArrayReverse=cardArray.reverse();
+    console.log(cardArrayReverse);
+    cardArrayReverse=cardArrayReverse.map(function (digits,numbers) {
+      if (numbers %2==!0){
+        const duplicatedDigits=digits*2;
+        if (duplicatedDigits>=10){
+          return duplicatedDigits-9;
+        }
+        return duplicatedDigits;
+      }
+      return parseInt(digits);
+      
+    });
+    console.log(cardArrayReverse);
+    // 1º iteración: mult posiciones pares x2
+    // map() solo se usa con arrays
+    let finalSumResult=0; 
+    cardArrayReverse.map(function (num) {
+      finalSumResult=finalSumResult+num
+      //finalSumResult+=num
+    })
+    console.log(finalSumResult);
+
+    if (finalSumResult%10===0) {
+      return true;
+    } 
+    return false;
+  },
+  
+  //finalSum:function (cardArrayReverse) {
+    //let cardDigitSum=cardArrayReverse.
+    
+  //},
+
+  maskify: function() {
+    let card=document.getElementById("cardNumber").value;
+    
+    card=card.split("")
+    for (let index = 0; index < card.length-4; index++) {
+      card[index]="#";
+      
+    }
+    card=card.join("") //string
+    console.log(card);
+
+ 
+    document.getElementById("maskedNumber").innerText=card;
+    //maskedNumber es mi boton guardar
+
+
+    
+  },
+  //mena: "ayudaaaaa"
 };
+
+
+/*const ayuda=document.getElementById("cardNumber");
+console.log(Object.values(ayuda));
+
+
+const ayudaAgain=Array.from(document.getElementById("cardNumber"));
+console.log(Object.values(ayudaAgain));
+
+const ayudaAgainAgain=Object.values(document.getElementById("cardNumber"));
+console.log(ayudaAgainAgain);
+
+const card=document.getElementById("cardNumber").value;
+console.log(card);*/
 
 export default validator;
