3db42dc059624822ce672571db1a8c3aa7f86439
JIMENA VILLALOBOS <jimenavillalobos@MacbookPROdeDALIA.local>
2022-12-12 12:37:07 -0600

    final changes

---
+++ docs/index.js
@@ -0,0 +1,58 @@
+import validator from "./validator.js";
+
+console.log(validator);
+
+validator.maskify()
+
+//console.log(validator.mena)
+
+document.getElementById("maskify").addEventListener("click",isValid);
+
+document.getElementById("maskify").addEventListener("click",validator.maskify);
+
+document.addEventListener("input",showCard);
+function showCard(event){
+  let inputCardNumber=event.target.value;
+  
+  inputCardNumber=inputCardNumber.replace(/[^0-9]+/g, "");
+  console.log(inputCardNumber);
+
+  document.getElementById("cardNumber").value=inputCardNumber;
+
+  console.log(event.target.value);
+  document.getElementById("maskedNumber").innerText=inputCardNumber;
+  //maskedNumber es mi boton guardar
+
+}
+
+function isValid(){ //Cambiar handleValidate por isValid porque así pide la descripción
+  
+  const showIfIsValid=validator.validateCard();
+  let message="";
+  let backgroundColor="";
+  let border="";
+  console.log(showIfIsValid);
+
+  
+  const validCard=document.getElementById("validOrInvalid");
+  if (showIfIsValid) {
+    message=" ✅ ¡Tarjeta válida! 🥳"
+    backgroundColor="#8af293"
+    border="2px solid #0d7504"
+  
+  } else{
+    message="🚫 Lo sentimos, tu tarjeta no es válida 😢"
+    backgroundColor="#f2988a"
+    border="2px solid #9e1b06"
+  }
+
+  validCard.innerText=message;
+  validCard.style.backgroundColor=backgroundColor;
+  validCard.style.display="block";
+  validCard.style.border=border;
+}
+
+
+
+
+
